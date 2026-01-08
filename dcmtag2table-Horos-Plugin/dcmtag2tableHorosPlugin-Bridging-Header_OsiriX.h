f38d21431ec0876aa1e922efcc032e2a0bb7b08a
Thales Matheus <1417625@sga.pucminas.br>
2026-01-07 21:03:14 -0600

    Add Xcode project files and bridging headers
    Added separate Xcode project files for Horos and OsiriX targets, along with corresponding Swift bridging headers to expose required Objective-C APIs. This sets up the initial structure for building the dcmtag2tableHorosPlugin for both platforms.

---
+++ dcmtag2table-Horos-Plugin/dcmtag2tableHorosPlugin-Bridging-Header_OsiriX.h
@@ -0,0 +1,15 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <OsiriXAPI/PluginFilter.h>
+#import <OsiriXAPI/BrowserController.h>
+#import <OsiriXAPI/DicomStudy.h>
+#import <OsiriXAPI/DicomSeries.h>
+#import <OsiriXAPI/DicomImage.h>
+#import <OsiriXAPI/dicomFile.h>
+#import <OsiriXAPI/DicomDatabase.h>
+#import <OsiriXAPI/ViewerController.h>
+#import <OsiriXAPI/DCMPix.h>
+#import <OsiriXAPI/ThreadsManager.h>
+#import "DCMObject.h"
